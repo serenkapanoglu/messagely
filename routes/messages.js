b829bccb0f9695cfeb6737564a7ff1911f869559
serenkapanoglu <70541836+serenkapanoglu@users.noreply.github.com>
2023-08-30 19:47:15 -0400

    Add files via upload

---
+++ routes/messages.js
@@ -0,0 +1,42 @@
+const Router = require("express").Router;
+const User = require("../models/user");
+const {ensureLoggedIn, ensureCorrectUser} = require("../middleware/auth");
+
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+router.get("/:id", ensureCorrectUser, async(req,res,next)=>{
+    try{
+        let messageId = await User.get(req.params.id);
+        return res.json({message});
+    }
+    catch(err) {
+        return next(err);
+    }
+});
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+
